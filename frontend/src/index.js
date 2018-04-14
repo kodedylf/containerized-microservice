1c593f13a767e617e847962e70021cf2a95bfa5b
Hans Kilian <hans@kilianconsult.dk>
2018-04-14 18:36:25 +0200

    Menu points for swagger and kibana

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -88,9 +88,9 @@ class Navbar extends React.Component {
           </div>
           <div id="navbar" className="collapse navbar-collapse">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
+              <li className="active"><a href="/">Home</a></li>
+              <li><a href="/swagger">API</a></li>
+              <li><a href="/log">Log-</a></li>
             </ul>
           </div>
         </div>
