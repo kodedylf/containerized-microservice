013fb548602b0443a03eeaa3ca97728d23f42537
Hans Kilian <hans@kilianconsult.dk>
2018-04-21 10:46:27 +0200

    Upgraded to Bootstrap 4.0 which is now included in bundle.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Bootstrap from 'bootstrap';
+import BootstrapCSS from 'bootstrap/dist/css/bootstrap.min.css';
 
 class Personlist extends React.Component {
   constructor(props) {
@@ -75,24 +76,24 @@ class InputForm extends React.Component {
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Containerized microservice</a>
-          </div>
-          <div id="navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="/">Home</a></li>
-              <li><a href="/swagger">API</a></li>
-              <li><a href="/log">Log-</a></li>
-            </ul>
-          </div>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a className="navbar-brand" href="#">Containerized microservice</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/swagger">API</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/log">Log</a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
