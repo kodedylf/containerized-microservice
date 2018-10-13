2d12da1ac3b410d62ddd6141c4999caff3a4c208
Hans Kilian <hans@kilianconsult.dk>
2018-10-14 01:00:52 +0200

    Create local user when logging in with Google auth token

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Google.Apis.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
 
 namespace postgres_service.Controllers
 {
@@ -12,18 +16,44 @@ namespace postgres_service.Controllers
     public class AuthController : Controller
     {
         private readonly ILogger _logger;
-        public AuthController(ILogger<AuthController> logger)
+        private readonly Repository _repository;
+        public AuthController(ILogger<AuthController> logger, Repository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
         
         [Route("google/{token}")]
         [HttpGet]
-        public async Task<string> LoginWithGoogleToken(string token)
+        public async Task<IActionResult> LoginWithGoogleToken(string token)
         {
             _logger.LogInformation(token);
             var payload = await GoogleJsonWebSignature.ValidateAsync(token);
-            return "j.w.t";
+
+            var user = _repository.Users.SingleOrDefault(u => u.GoogleUserId == payload.Subject);
+            if (user == null) {
+                user = new User {
+                    GoogleUserId = payload.Subject,
+                    Email = payload.Email,
+                    GivenName = payload.GivenName,
+                    FamilyName = payload.FamilyName,
+                    Roles = new string[0]
+                };
+                _repository.Users.Add(user);
+                _repository.SaveChanges();
+            }
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.GivenName, user.GivenName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, user.FamilyName)
+            };
+            claims = claims.Concat(user.Roles.Select(r => new Claim(ClaimTypes.Role, r.ToString()))).ToArray();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecretKey"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var jwtToken = new JwtSecurityToken("issuer", "issuer", claims, expires: DateTime.Now.AddMinutes(30), signingCredentials: creds);
+            return Ok(new JwtSecurityTokenHandler().WriteToken(jwtToken));
         }
     }
 }
--- backend/repository/repo.cs
+++ backend/repository/repo.cs
@@ -1,14 +1,27 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 public class Repository : DbContext {
     public Repository(DbContextOptions<Repository> options) : base(options) { }
     public DbSet<Person> Personer { get; set; }
+    public DbSet<User> Users { get; set; }
 }
 
 public class Person {
     [Key]
     public int PersonId { get; set; }
     public string Navn { get; set; }
+}
+
+public class User {
+    [Key]
+    public int UserId { get; set; }
+    public string GoogleUserId { get; set; }
+    public string Email { get; set; }
+    public string[] Roles { get; set; }
+    public string GivenName { get; internal set; }
+    public string FamilyName { get; internal set; }
 }
